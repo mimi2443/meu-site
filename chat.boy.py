5a3a49d325c77ed4970ebffdab871404d5621100
mimi2443 <yasmimmaciel794@gmail.com>
2025-11-03 11:20:38 -0300

    Add files via upload

---
+++ chat.boy.py
@@ -0,0 +1,23 @@
+print("chatboy: ola! Eu sou o chatbot da empresa.")
+
+pergunta = input("Voce: ").lower
+if "oi" in user_input or "olá" in user_input:
+        print("ChatBoy: Oi! Tudo bem?")
+    elif "tudo bem" in user_input:
+        print("ChatBoy: Estou ótimo, obrigado por perguntar!")
+    elif "qual seu nome" in user_input:
+        print("ChatBoy: Eu sou o ChatBoy, seu assistente virtual.")
+    elif "adeus" in user_input or "tchau" in user_input:
+        print("ChatBoy: Até mais! Foi bom conversar com você.")
+        break
+         elif "qual seu horário de funcionamento" in user_input:
+        print("ChatBoy:Seg á Sab:Das 9h á 22h30,Dom:Das 14h30 á 23h")
+    else:
+        elif 'qual a localização?' in user_input:
+            print('Praça Bernadinho de Lima,186,Centro Nova Lima')
+        elif ''
+        print("ChatBoy: Desculpe, não entendi. Pode repetir?")
+else:
+print("chatbot: Desculpe, nao entendi.")
+
+
